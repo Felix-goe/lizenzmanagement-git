f50328556616a2b9591b077361bef2db54a68425
Felix Lorenzen <felix.lorenzen01@stud.uni-goettingen.de>
2024-12-10 21:42:26 +0100

    erste aenderungen

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Willkommen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="styelsheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+
+    <div class="container min-vh-100 d-flex justify-content-center align-items-center">
+
+  <h1><b> Das ist die Homepage</b> </h1>
+
+ <h2> <a href="/lizenzen_user">Lizenzen User</a> </h2>
+
+    </div>
+
+  </body>
+</html>
