11c9eccdaca658e6895ca39e9dd0c755ea576aa7
Felix Lorenzen <felix.lorenzen01@stud.uni-goettingen.de>
2024-12-20 13:39:39 +0100

    admin use blade2

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -123,7 +123,7 @@
                                                 <td>101</td>
                                                 <td>1234</td>
                                                 <td>Informatik</td>
-                                                <td></td>
+                                                <td>informatik@test.de</td>
                                                 <td>+49 123 456789</td>
                                                 <td>
                                                     <button id="edit1"><i
@@ -136,7 +136,7 @@
                                                 <td>102</td>
                                                 <td>5678</td>
                                                 <td>BWL</td>
-                                                <td></td>
+                                                <td>bwl@test.de</td>
                                                 <td>+49 987 654321</td>
                                                 <td>
                                                     <button id="edit2"><i
