7c9e42eae2b1c33a3d36f6f21879253238113579
ErikSwk <erik.schweckendiek01@stud.uni-goettingen.de>
2025-03-03 17:00:06 +0100

    Update admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,7 +10,6 @@
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
     <!--google fonts -->
